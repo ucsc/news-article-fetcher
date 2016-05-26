98ec79a9ea3a255c5f28dec6d6a6353f7120373e
Philip Guther <philip@guther.com>
2016-05-26 11:18:41 -0700

    divided downloading and saving training sets into two different sections. added functions to convert a training_dictionary to xtrain and ytrain lists, and to slice those lists into seperate sections for kfold validation.

--- classifier.py
+++ classifier.py
@@ -6,6 +6,8 @@ import re
 from scraper import NewsSiteScraper
 from utils import CommandLineDisplay
 
+from sklearn.preprocessing import MultiLabelBinarizer
+
 
 class ArticleClassifier(object):
     """
@@ -26,35 +28,27 @@ class ArticleClassifier(object):
         self.metadata_regex = re.compile(r"^---classification-training-metadata---$")
         self.category_regex = re.compile(r"^category: (.+)$")
 
-    def download_training_set(self):
+    def save_training_set(self, training_dictionary, path='training_articles/'):
         """
-        Downloads a set of training data consisting of all news.ucsc.edu articles and stores them
-        in subfolders of year and month in a directory called training_articles/
+        Takes a dictionary of training articles and saves them to the directory
+        indicated in the path. Creates the path of it doesn't exist.
+        :param path:
+        :param training_dictionary:
         :return:
         """
-        base_folder = 'training_articles/'
-        articles_dictionary = self.article_scraper.get_articles_dictionary()
-        print "Writing Training Articles..."
+        if os.path.exists(path):
+            if not os.path.isdir(path):
+                print 'Path is not a directory, unable to save'
+                return
+        else:
+            os.makedirs(path)
 
-        if not os.path.exists(base_folder):
-            os.makedirs(base_folder)
-
-        for article_url, article_dict in articles_dictionary.iteritems():
+        print 'Saving Training Set...'
+        for filename, article_dict in training_dictionary:
             categories = article_dict['categories']
             article_body = article_dict['article_body']
-            date = article_dict['date']
-            file_name = article_dict['file_name']
 
-            date_object = datetime.datetime.strptime(date, "%Y-%m-%d")
-
-            article_year_folder = date_object.strftime("%Y") + '/'
-
-            article_month_folder = date_object.strftime("%m") + '/'
-
-            if not os.path.exists(base_folder + article_year_folder + article_month_folder):
-                os.makedirs(base_folder + article_year_folder + article_month_folder)
-
-            fo = open(base_folder + article_year_folder + article_month_folder + file_name, "w")
+            fo = open(filename, "w")
 
             fo.write('---classification-training-metadata---\n')
             for category in categories:
@@ -63,12 +57,33 @@ class ArticleClassifier(object):
             fo.write('---classification-training-metadata---\n')
 
             fo.write(article_body + '\n')
-
             fo.close()
+        print 'Done'
 
-        print "Done"
+    def download_training_set(self):
+        """
+        Downloads a set of training data consisting of all news.ucsc.edu articles and stores them
+        in subfolders of year and month in a directory called training_articles/
+        :return:
+        """
+        base_folder = 'training_articles/'
+        articles_dictionary = self.article_scraper.get_articles_dictionary()
+        training_dictionary = dict()
+        print "Writing Training Articles..."
 
-    def read_training_set(self, training_set_path='training_articles/'):
+        if not os.path.exists(base_folder):
+            os.makedirs(base_folder)
+
+        for article_url, article_dict in articles_dictionary.iteritems():
+            categories = article_dict['categories']
+            article_body = article_dict['article_body_no_html']
+            file_name = article_dict['file_name']
+
+            training_dictionary[file_name] = {'categories': categories,
+                                              'article_body': article_body}
+        return training_dictionary
+
+    def read_training_dictionary(self, training_set_path='training_articles/'):
         """
         reads all articles in the given directory and returns a dictionary of dictionaries, where each
         key is a training article path, and each value is a dictionary consisting of a list of the
@@ -120,10 +135,66 @@ class ArticleClassifier(object):
                     articles_dictionary[file_path] = article_dict
         return articles_dictionary
 
-    def kfold_validation(self, k, training_set):
+    def dictionary_to_xytrain(self, training_dictionary):
+        """
+        Takes a dictionary of training articles and splits them up into xtrain and ytrain sets. Also returns
+        a dictionary of indexes and their corresponding categories.
+        :param training_dictionary:
+        :return:
+        """
+        cat_num = 0
+
+        xtrain = []
+        categories_list = []
+        categories_dict = dict()
+
+        for filename, article_dict in training_dictionary.iteritems():
+            categories = article_dict['categories']
+            article_body = article_dict['article_body']
+
+            # print article_body
+            categories_sub_list = []
+
+            xtrain.append(article_body)
+
+            for category in categories:
+                if category not in categories_dict:
+                    categories_dict[category] = cat_num
+                    cat_num += 1
+
+                categories_sub_list.append(categories_dict[category])
+
+            categories_list.append(categories_sub_list)
+
+        ytrain = MultiLabelBinarizer().fit_transform(categories_list)
+
+        inv_categories_dict = {v: k for k, v in categories_dict.items()}
+
+        return xtrain, ytrain, inv_categories_dict
+
+    def slice_training_set(self, xtrain, ytrain, slice_start, slice_end):
+        """
+        Takes the x and y training lists, and slices them into two sets (4 total).  The list from the
+        start index (inclusive) to the end index (exclusive) forms one of the new sets, and the other elements
+        form the other.
+        :param xtrain:
+        :param ytrain:
+        :param slice_start:
+        :param slice_end:
+        :return:
+        """
+        inner_xtrain = xtrain[slice_start:slice_end]
+        inner_ytrain = ytrain[slice_start:slice_end]
+
+        outer_xtrain = xtrain[:slice_start] + xtrain[slice_end:]
+        outer_ytrain = ytrain[:slice_start] + ytrain[slice_end:]
+
+        return inner_xtrain, inner_ytrain, outer_xtrain, outer_ytrain
+
+    def kfold_validation(self, k, training_dictionary):
         """
         performs k fold validation on the given training set and prints the statistics.
         :param k:
-        :param training_set:
+        :param training_dictionary:
         :return:
         """
